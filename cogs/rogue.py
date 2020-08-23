6a7e59bf6935091112a270ea5d979fec4c5e97a1
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-08-22 20:12:54 -0700

    Update rogue.py

--- cogs/rogue.py
+++ cogs/rogue.py
@@ -3,7 +3,8 @@ import discord
 from discord.ext import commands
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-start_rogue_tracking = None
+is_tracking_rogue = None
+items_to_check = []
 
 
 class Rogue(commands.Cog):
@@ -14,21 +15,53 @@ class Rogue(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def startrogue(self, ctx):
-        global start_rogue_tracking
-        start_rogue_tracking = True
-        await ctx.send(f'Starting Rogue Tracking?: {start_rogue_tracking}')
+        await ctx.message.delete()
+        global is_tracking_rogue, items_to_check
+
+        is_tracking_rogue = True
+
+        start_msg = await ctx.send(f'Starting Rogue Tracking?: {is_tracking_rogue}')
+
+        embed_description = 'Please enter all Rogue items you want tracked separated by line breaks.'
+        embed_msg = discord.Embed(title='Rogue Tracking', color=5111552, description=embed_description)
+        rogue_items_to_track = await ctx.send(embed=embed_msg)
+        timeout_time = 30
+        timeout_msg = 'Timed Out or Invalid Input.'
+
+        try:
+            items_response = await self.client.wait_for('message', timeout=timeout_time)
+        except:
+            await ctx.send(timeout_msg)
+            await items_response.delete()
+            await start_msg.delete()
+            is_tracking_rogue = False
+            stop_msg = await ctx.send(f'Starting Rogue Tracking?: {is_tracking_rogue}')
+        else:
+            for item in items_response.content.split('\n'):
+                if len(item) <= 0 or item[0] == '#' or item[0:2] == '//':
+                    continue
+                elif item not in items_to_check:
+                    items_to_check.append(item)
+
+            # await items_response.delete()
+            # await rogue_items_to_track.edit(embed=embed_msg)
+            await ctx.send(items_to_check)
+
+
+
+
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def stoprogue(self, ctx):
-        global start_rogue_tracking
-        start_rogue_tracking = False
-        await ctx.send(f'Starting Rogue Tracking?: {start_rogue_tracking}')
+        global is_tracking_rogue
+        is_tracking_rogue = False
+        await ctx.send(f'Starting Rogue Tracking?: {is_tracking_rogue}')
 
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def statusrogue(self, ctx):
-        await ctx.send(f'Starting Rogue Tracking?: {start_rogue_tracking}')
+        await ctx.send(f'Starting Rogue Tracking?: {is_tracking_rogue}')
 
 
 def setup(client):
