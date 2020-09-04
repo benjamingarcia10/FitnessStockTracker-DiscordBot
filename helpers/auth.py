efc43533e706c55111f6c2de0b11d27255e3c6a3
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-09-03 21:11:06 -0700

    Added global authorization check for basic commands

--- helpers/auth.py
+++ helpers/auth.py
@@ -2,9 +2,12 @@ import variables
 from discord.ext import commands
 
 developer_id = 433083891109330945
+allowed_commands = ['help']
 
 
 async def is_authorized(ctx):
+    if ctx.command.name in allowed_commands:
+        return True
     if ctx.author.permissions_in(ctx.channel).administrator:
         return True
     if ctx.author.id == developer_id:
