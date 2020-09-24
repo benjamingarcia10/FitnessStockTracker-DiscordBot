89242c0caaa96cd7421c25c921156cf4e0e7a81d
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-09-24 15:40:53 -0700

    Updated to include reddit user agent outside of env file

--- helpers/redditNotify.py
+++ helpers/redditNotify.py
@@ -10,7 +10,7 @@ def notify_stock_reddit_submission(description):
     try:
         reddit = praw.Reddit(client_id=os.getenv('REDDIT_CLIENT_ID'),
                              client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-                             user_agent=os.getenv('REDDIT_USER_AGENT'),
+                             user_agent='windows:com.redditbot.roguestocknotifier:v1 (by /u/bennykgarcia)',
                              username=os.getenv('REDDIT_USERNAME'),
                              password=os.getenv('REDDIT_PASSWORD'))
 
