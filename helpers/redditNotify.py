9f2c2c6e5d0cc63e85d95f0d82bc2d34f917a59f
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-09-24 17:50:34 -0700

    Added comments

--- helpers/redditNotify.py
+++ helpers/redditNotify.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 
+# Post notification to r/homegym Stock and Shipping pinned/stickied thread
 def notify_stock_reddit_submission(description):
     stock_thread = None
     try:
@@ -14,8 +15,10 @@ def notify_stock_reddit_submission(description):
                              username=os.getenv('REDDIT_USERNAME'),
                              password=os.getenv('REDDIT_PASSWORD'))
 
+        # Retrieve r/homegym subreddit object
         subreddit = reddit.subreddit('homegym')
 
+        # Get the stickied thread with Stock and Shipping in title
         for submission in subreddit.hot(limit=5):
             if 'stock and shipping' in submission.title.lower().strip() and submission.stickied:
                 stock_thread = submission
@@ -23,6 +26,7 @@ def notify_stock_reddit_submission(description):
         if stock_thread is None:
             print(f'Stock thread not found')
             return
+        # Post reply based on passed in argument
         stock_thread.reply(description)
     except Exception as e1:
         print(f'{type(e1)} - {e1}: Unable to post Reddit reply')
